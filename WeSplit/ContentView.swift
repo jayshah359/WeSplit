63f708d4882bb04ec4061316156b3580db9fdf3a
Jay Shah <jayshah@yahoo.com>
2019-12-04 00:43:51 -0800

    Initial Commit

---
+++ WeSplit/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  WeSplit
+//
+//  Created by Jaydev Shah on 12/4/19.
+//  Copyright © 2019 Jaydev Shah. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
